0d1405b574c9b2abc35479366fbcd42d353e9fea
achu <chue0927@gmail.com>
2025-05-07 14:02:33 +0800

    whitelist_id_change

--- core/__whitelist__.py
+++ core/__whitelist__.py
@@ -1,10 +1,14 @@
 import discord
+import json
+with open('./json/setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
 
 class mywhite():
     async def iswhitelist(interaction: discord.Interaction):  
         if interaction.user.guild_permissions.administrator:  
             return True
-        target_role_id = 1133754686470627350
+        target_role_id = jdata['white_ID']
         member_roles = [role.id for role in interaction.user.roles]  
 
         if target_role_id in member_roles:
