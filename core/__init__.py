ab1dac73b0556b9d57f09f155218232e9c5c3869
achu <chue0927@gmail.com>
2026-03-25 11:19:59 +0800

    __init__ fix

--- core/__init__.py
+++ core/__init__.py
@@ -2,9 +2,12 @@ import discord
 from discord.ext import commands
 from holodex.client import HolodexClient
 
+from core import __json__
+jdata = __json__.load_json('./json/setting.json')
+
 class Cog_Extension(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        self.holodex_client = HolodexClient('df1fab08-008b-42cf-8620-4a8776e9dd0d')
-        self.holodex_client_2 = HolodexClient('5c22346d-1381-4a29-96c6-739814d36006')
-        self.holodex_client_3 = HolodexClient('4372f53a-5865-431c-97d2-313ce173c479')
+        self.holodex_client = HolodexClient(jdata['HOLODEX_API_KEY'])
+        self.holodex_client_2 = HolodexClient(jdata['HOLODEX_API_KEY_2'])
+        self.holodex_client_3 = HolodexClient(jdata['HOLODEX_API_KEY_3'])
